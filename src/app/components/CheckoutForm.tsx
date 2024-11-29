1fb8dd43a919ea9094bdb0e1026f349c94b6048f
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2024-11-29 17:02:04 +0000

    Auto-commit: 2024-11-29 17:02:04

--- src/app/components/CheckoutForm.tsx
+++ src/app/components/CheckoutForm.tsx
@@ -1,65 +1,40 @@
+"use client";
+
 import React from "react";
 
 import { PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
 
-import value from "./OrderSummary";
-import { NextApiRequest, NextApiResponse } from "next";
-
 const CheckoutForm = () => {
-  const handleSubmit = async (req: NextApiRequest, res: NextApiResponse) => {
-    if (req.method === "POST") {
-      try {
-        const data = req.body;
-        const cliente = await prisma.cliente.create({
-          data: {
-            nome: data.nome,
-            morada: data.morada,
-            cpostal: data.cpostal,
-            email: data.email,
-            telemovel: data.telemovel,
-            quantidade: value,
-          },
-        });
-        res.status(201).json({ message: "Form submitted successfully" });
-      } catch (error) {
-        console.error("Error submitting form:", error);
-        res.status(500).json({ message: "Error submitting form" });
-      }
-    } else {
-      res.status(405).json({ message: "Method not allowed" });
-    }
-  };
+  const handleSubmit = async (formData: FormData) => {
+    const nome = formData.get("nome") as string;
+    const morada = formData.get("morada") as string;
+    const cpostal = formData.get("cpostal") as string;
+    const email = formData.get("email") as string;
+    const telemovel = formData.get("telemovel") as string;
+    const quantidade = formData.get("quantidade") as string;
 
+    console.log(nome, morada, cpostal, email, telemovel, quantidade);
+  };
   return (
     <div className="lg:col-span-2 col-span-4 bg-white space-y-8 px-12">
-      <form id="payment-form" method="POST" action="" onSubmit={handleSubmit}>
+      <form id="payment-form" method="POST" action={handleSubmit}>
         <h2 className="uppercase tracking-wide text-lg font-semibold text-gray-700 my-2">
           Pagamento e Dados de Envio
         </h2>
         <fieldset className="mb-3 bg-white shadow-lg rounded text-gray-600">
           <label className="flex border-b border-gray-200 h-12 py-3 items-center">
             <span className="text-right px-2">Nome</span>
-            <input
-              id="name"
-              name="name"
-              className="focus:outline-none px-3"
-              required
-            />
+            <input name="nome" className="focus:outline-none px-3" required />
           </label>
           <label className="flex border-b border-gray-200 h-12 py-3 items-center">
             <span className="text-right px-2">Morada</span>
-            <input
-              id="morada"
-              name="morada"
-              className="focus:outline-none px-3"
-              required
-            />
+            <input name="morada" className="focus:outline-none px-3" required />
           </label>
           <label className="flex border-b border-gray-200 h-12 py-3 items-center">
             <span className="text-right px-2">C. Postal</span>
             <input
-              id="cpostal"
               name="cpostal"
               className="focus:outline-none px-3"
               placeholder="1234-123"
@@ -69,7 +44,6 @@ const CheckoutForm = () => {
           <label className="flex border-b border-gray-200 h-12 py-3 items-center">
             <span className="text-right px-2">Email</span>
             <input
-              id="email"
               name="email"
               type="email"
               className="focus:outline-none px-3"
@@ -80,7 +54,6 @@ const CheckoutForm = () => {
           <label className="flex border-b border-gray-200 h-12 py-3 items-center">
             <span className="text-right px-2">Telemóvel</span>
             <input
-              id="telemovel"
               name="telemovel"
               className="focus:outline-none px-3"
               required
