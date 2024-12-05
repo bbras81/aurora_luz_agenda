f899a3c562a7ffc0e1c040025b25fc739df74946
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2024-12-05 12:57:54 +0000

    Auto-commit: 2024-12-05 12:57:54

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/app/components/Navbar";
-import Head from "./components/Head";
+import Script from "next/script";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Aurora de Luz",
@@ -15,8 +16,37 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-PT">
-      <Head />
       <body className="bg-background">
+        {/* Script do Pixel do Meta */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${1742898359892269}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <Image
+            src={`https://www.facebook.com/tr?id=${1742898359892269}&ev=PageView&noscript=1`}
+            alt="Pixel do Meta"
+            width={1}
+            height={1}
+            style={{ display: "none" }}
+          />
+        </noscript>
+        {/* Fim do Script do Pixel */}
+
         <Navbar />
         {children}
       </body>
